d962ddef56eaa164913c753b5e12a8bbc5203dce
NubieMe <fahmi.mellow@gmail.com>
2024-06-15 10:33:17 +0700

    update: add error handler to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { routes } from "./routes";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", routes);
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log("server running on port 3000");
