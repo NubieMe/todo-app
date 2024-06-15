85cafd65fb8c47e243b6361ca54d4e1693662638
NubieMe <fahmi.mellow@gmail.com>
2024-06-15 11:25:02 +0700

    update: minor adjustment in user model

--- prisma/migrations/20240615041928_/migration.sql
+++ prisma/migrations/20240615041928_/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
 CREATE TABLE `users` (
-    `username` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(100) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
-    `token` VARCHAR(255) NOT NULL,
+    `token` VARCHAR(255) NULL,
 
     UNIQUE INDEX `users_username_key`(`username`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
