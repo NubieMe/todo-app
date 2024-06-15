fa65a988c10ab71d35669f88d49833b598fc32cc
NubieMe <fahmi.mellow@gmail.com>
2024-06-15 20:35:56 +0700

    fix: fixing pathname & middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,20 +2,21 @@ import express from "express";
 import { UserController } from "../controllers/user-controller";
 import { CategoryController } from "../controllers/category-controller";
 import { TodoController } from "../controllers/todo-controller";
+import { auth } from "../middlewares/auth";
 
 export const routes = express.Router();
 
 // User
-routes.post("/api/register", UserController.register);
-routes.post("/api/login", UserController.login);
+routes.post("/register", UserController.register);
+routes.post("/login", UserController.login);
 
 // Category
-routes.post("/api/category", CategoryController.create);
-routes.get("/api/category", CategoryController.getAll);
-routes.delete("/api/category/:id", CategoryController.delete);
+routes.post("/category", CategoryController.create);
+routes.get("/category", CategoryController.getAll);
+routes.delete("/category/:id", CategoryController.delete);
 
 // Todo
-routes.post("/api/todo", TodoController.create);
-routes.get("/api/todo", TodoController.getTodo);
-routes.put("/api/todo/:id", TodoController.update);
-routes.delete("/api/todo/:id", TodoController.delete);
+routes.post("/todo", TodoController.create);
+routes.get("/todo", TodoController.getTodo);
+routes.put("/todo/:id", auth, TodoController.update);
+routes.delete("/todo/:id", auth, TodoController.delete);
